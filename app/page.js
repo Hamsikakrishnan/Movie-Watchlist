2aeaf88a51987bc54d47a2983ddc0d88fa7455a3
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-02-13 17:38:57 +0530

    Connected Database

--- app/page.js
+++ app/page.js
@@ -1,7 +1,44 @@
-import Image from "next/image";
+"use client";
 
+import {db} from "../firebaseConfig";
+import {addDoc, collection, getDocs} from "firebase/firestore";
+import {useEffect, useState} from "react";
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  // const [movie, setMovie] = useState({
+  //   movie : "",
+  //   genre: "",
+  //   description: "",
+  // });
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "not_watched_movies"));
+        const movieList = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        console.log("Fetched Movies:", movieList);  // Debugging log
+        setMovies(movieList);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    getMovies();
+  }, []);
+  
+
   return (
-    <h1>This is the starting of the Movie-Watchlist</h1>
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id} className="flex text-black items-center justify-between p-2 border-b">
+            
+                <span className="text-xl">{movie.movie}</span>
+                <span className="text-xl">{movie.genre}</span>
+                <span className="text-xl">{movie.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+    
   );
 }
