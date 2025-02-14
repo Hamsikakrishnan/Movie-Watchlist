e8e37bc59b00881a4ba469302b967e2fe47f428c
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-02-14 15:37:12 +0530

    Nav bar and somechanges

--- app/page.js
+++ app/page.js
@@ -27,10 +27,11 @@ export default function Home() {
   
 
   return (
-    <div>
+    <div className="bg-black font-mono">
+      <button className="bg-blue-300 py-3 px-5 m-5 font-bold text-lg font-mono rounded-md">Add new Movie<br/> to Watchlist</button>
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id} className="flex text-black items-center justify-between p-2 border-b">
+          <li key={movie.id} className="flex text-white items-center justify-between p-2 border-b">
             
                 <span className="text-xl">{movie.movie}</span>
                 <span className="text-xl">{movie.genre}</span>
